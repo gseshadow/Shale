c0fd466f36f91de70a7c7200d27fa1419f0d4b7a
Brian Downing <downingb@unm.edu>
2026-03-25 15:49:40 -0600

    Make assigned cases panel scroll independently in user view

---
+++ shale-ui/src/main/java/com/shale/ui/services/UserDetailService.java
@@ -0,0 +1,52 @@
+package com.shale.ui.services;
+
+import java.util.List;
+import java.util.Objects;
+
+import com.shale.data.dao.CaseDao;
+import com.shale.data.dao.CaseDao.CaseRow;
+import com.shale.data.dao.UserDao;
+import com.shale.data.dao.UserDao.UserDetailRow;
+import com.shale.data.dao.UserDao.UserProfileUpdateRequest;
+import com.shale.data.dao.UserDao.UserRoleRow;
+
+public final class UserDetailService {
+
+	private static final int ASSIGNED_CASES_LIMIT = 25;
+
+	private final UserDao userDao;
+	private final CaseDao caseDao;
+
+	public UserDetailService(UserDao userDao, CaseDao caseDao) {
+		this.userDao = Objects.requireNonNull(userDao, "userDao");
+		this.caseDao = Objects.requireNonNull(caseDao, "caseDao");
+	}
+
+	public UserDetailRow loadUser(int userId, int shaleClientId) {
+		return userDao.findById(userId, shaleClientId);
+	}
+
+	public List<UserRoleRow> loadAssignedRoles(int targetUserId, int shaleClientId) {
+		return userDao.listAssignedRoles(targetUserId, shaleClientId);
+	}
+
+	public List<UserRoleRow> loadAssignableRoles(int targetUserId, int shaleClientId) {
+		return userDao.listAssignableRoles(targetUserId, shaleClientId);
+	}
+
+	public boolean updateBasicProfile(UserProfileUpdateRequest request) {
+		return userDao.updateBasicProfile(request);
+	}
+
+	public boolean addRoleToUser(int userId, int roleId, int shaleClientId) {
+		return userDao.addRoleToUser(userId, roleId, shaleClientId);
+	}
+
+	public boolean removeRoleFromUser(int userId, int roleId, int shaleClientId) {
+		return userDao.removeRoleFromUser(userId, roleId, shaleClientId);
+	}
+
+	public List<CaseRow> loadAssignedCases(int userId) {
+		return caseDao.listActiveCasesForUserTeamMember(userId, ASSIGNED_CASES_LIMIT);
+	}
+}
