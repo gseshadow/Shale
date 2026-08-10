a5750a3f3f1827f7bfaf948719ecc48d2ebe3fb2
Brian Downing <downingb@unm.edu>
2026-08-10 16:20:03 -0600

    Fix tenant 7 Case Date Type inventory IDs

--- docs/sql/verification/2026-08-10_case_date_type_ownership_inventory.sql
+++ docs/sql/verification/2026-08-10_case_date_type_ownership_inventory.sql
@@ -14,11 +14,11 @@ INSERT @Noncritical VALUES
 
 SELECT N'01_DEFINITIONS' SectionName,t.Id CaseDateTypeId,t.ShaleClientId TypeOwnerTenantId,t.SystemKey,t.Name,t.IsActive,t.IsDeleted,t.CreatedByUserId
 FROM dbo.CaseDateTypes t JOIN @Noncritical n ON n.SystemKey=t.SystemKey ORDER BY t.SystemKey,t.ShaleClientId,t.Id;
-SELECT N'02_OCCURRENCE_USAGE' SectionName,t.Id CaseDateTypeId,t.SystemKey,cd.ShaleClientId,COUNT_BIG(*) OccurrenceCount,
+SELECT N'02_OCCURRENCE_USAGE' SectionName,t.Id CaseDateTypeId,t.SystemKey,cd.ShaleClientId,COUNT_BIG(cd.Id) OccurrenceCount,
  SUM(CASE WHEN cd.IsDeleted=0 THEN 1 ELSE 0 END) ActiveOccurrenceCount,COUNT_BIG(DISTINCT cd.CaseId) CaseCount
 FROM dbo.CaseDateTypes t JOIN @Noncritical n ON n.SystemKey=t.SystemKey LEFT JOIN dbo.CaseDates cd ON cd.CaseDateTypeId=t.Id
 GROUP BY t.Id,t.SystemKey,cd.ShaleClientId ORDER BY t.SystemKey,t.Id,cd.ShaleClientId;
-SELECT N'03_FORM_REFERENCES' SectionName,t.Id CaseDateTypeId,t.SystemKey,f.ShaleClientId,COUNT_BIG(*) ConfiguredFieldCount,
+SELECT N'03_FORM_REFERENCES' SectionName,t.Id CaseDateTypeId,t.SystemKey,f.ShaleClientId,COUNT_BIG(f.Id) ConfiguredFieldCount,
  COUNT_BIG(DISTINCT f.FormConfigurationId) FormConfigurationCount
 FROM dbo.CaseDateTypes t JOIN @Noncritical n ON n.SystemKey=t.SystemKey LEFT JOIN dbo.FormConfiguredFields f ON f.CaseDateTypeId=t.Id
 GROUP BY t.Id,t.SystemKey,f.ShaleClientId ORDER BY t.SystemKey,t.Id,f.ShaleClientId;
@@ -30,4 +30,15 @@ SELECT N'05_SEMANTIC_ROLE_MAPPINGS' SectionName,m.ShaleClientId,m.SemanticRoleKe
 FROM dbo.CaseDateTypeSemanticRoleMappings m JOIN dbo.CaseDateTypes t ON t.Id=m.CaseDateTypeId ORDER BY m.SemanticRoleKey,m.ShaleClientId,m.Id;
 SELECT N'06_CROSS_TENANT_BLOCKERS' SectionName,cd.ShaleClientId CaseDateTenantId,t.ShaleClientId TypeOwnerTenantId,COUNT_BIG(*) RowCount
 FROM dbo.CaseDates cd JOIN dbo.CaseDateTypes t ON t.Id=cd.CaseDateTypeId
-WHERE t.ShaleClientId IS NOT NULL AND t.ShaleClientId<>cd.ShaleClientId GROUP BY cd.ShaleClientId,t.ShaleClientId;
+WHERE t.ShaleClientId IS NOT NULL AND (cd.ShaleClientId IS NULL OR t.ShaleClientId<>cd.ShaleClientId) GROUP BY cd.ShaleClientId,t.ShaleClientId;
+
+SELECT N'07_PROTECTED_GLOBALS' SectionName,t.Id,t.SystemKey,t.Name,t.ShaleClientId
+FROM dbo.CaseDateTypes t JOIN dbo.CaseDateTypeSemanticRoleMappings m ON m.CaseDateTypeId=t.Id
+WHERE m.ShaleClientId IS NULL AND m.IsActive=1 AND m.IsDeleted=0 ORDER BY m.SemanticRoleKey;
+SELECT N'08_TENANT7_REFERENCE_TOTALS' SectionName,
+ (SELECT COUNT_BIG(*) FROM dbo.CaseDates cd JOIN @Noncritical n ON EXISTS(SELECT 1 FROM dbo.CaseDateTypes t WHERE t.Id=cd.CaseDateTypeId AND t.SystemKey=n.SystemKey) WHERE cd.ShaleClientId=7) OccurrenceCount,
+ (SELECT COUNT_BIG(*) FROM dbo.FormConfiguredFields f JOIN @Noncritical n ON EXISTS(SELECT 1 FROM dbo.CaseDateTypes t WHERE t.Id=f.CaseDateTypeId AND t.SystemKey=n.SystemKey) WHERE f.ShaleClientId=7) FormReferenceCount;
+SELECT N'09_TENANT8_CUSTOM_TYPES' SectionName,Id,SystemKey,Name FROM dbo.CaseDateTypes WHERE ShaleClientId=8;
+SELECT N'10_FORM_CROSS_TENANT_BLOCKERS' SectionName,f.ShaleClientId,t.ShaleClientId TypeOwnerTenantId,COUNT_BIG(*) RowCount
+FROM dbo.FormConfiguredFields f JOIN dbo.CaseDateTypes t ON t.Id=f.CaseDateTypeId
+WHERE t.ShaleClientId IS NOT NULL AND (f.ShaleClientId IS NULL OR t.ShaleClientId<>f.ShaleClientId) GROUP BY f.ShaleClientId,t.ShaleClientId;
