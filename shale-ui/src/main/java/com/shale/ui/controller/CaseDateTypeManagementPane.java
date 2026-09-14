4465cc289833f683aabf44c0de31604130a50ab1
Brian Downing <downingb@outlook.com>
2026-09-14 11:55:09 -0600

    fixed import

--- shale-ui/src/main/java/com/shale/ui/controller/CaseDateTypeManagementPane.java
+++ shale-ui/src/main/java/com/shale/ui/controller/CaseDateTypeManagementPane.java
@@ -3,6 +3,7 @@ package com.shale.ui.controller;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.IntConsumer;
 
 import org.slf4j.Logger;
