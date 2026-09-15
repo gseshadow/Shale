6651a3d55743fda4bd44c2ecafd97c14741a9254
Brian Downing <downingb@unm.edu>
2026-09-15 10:03:36 -0600

    Fix user management test assertion imports

--- shale-ui/src/test/java/com/shale/ui/controller/SettingsControllerLifecycleTest.java
+++ shale-ui/src/test/java/com/shale/ui/controller/SettingsControllerLifecycleTest.java
@@ -1,6 +1,7 @@
 package com.shale.ui.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
